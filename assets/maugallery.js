3dda0ca03659257e99e39454720248f13e16dcc1
alex.vaslin <vaslinalex@gmail.com>
2024-05-22 15:05:26 +0200

    amelioration de l'accessibilité en JS et html

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -187,13 +187,13 @@
                         <div class="modal-body">
                             ${
                               navigation
-                                ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
+                                ? '<div class="mg-prev" aria-label="photo précédente" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
                                 : '<span style="display:none;" />'
                             }
                             <img class="lightboxImage img-fluid" alt="Contenu de l\'image affichée dans la modale au clique"/>
                             ${
                               navigation
-                                ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
+                                ? '<div class="mg-next" aria-label="photo suivante" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
                                 : '<span style="display:none;" />'
                             }
                         </div>
@@ -204,10 +204,10 @@
 
     showItemTags(gallery, position, tags) {
       // Affiche les tags de filtrage en fonction de leur position
-      var tagItems = '<li class="nav-item"><span class="nav-link active active-tag" data-images-toggle="all">Tous</span></li>';
+      var tagItems = '<li class="nav-item"><span class="nav-link active active-tag" data-images-toggle="all" aria-label="afficher toute la galerie">Tous</span></li>';
       $.each(tags, function(index, value) {
         tagItems += `<li class="nav-item">
-                <span class="nav-link" data-images-toggle="${value}">${value}</span></li>`;
+                <span class="nav-link" data-images-toggle="${value}" aria-label="Trier les images par ${value}">${value}</span></li>`;
       });
       var tagsRow = `<ul class="my-4 tags-bar nav nav-pills">${tagItems}</ul>`;
       if (position === "bottom") {
